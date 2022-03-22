d4ff369d95800e3bd5eddc67daf99d32cdcfe81b
aria6 <dian.ariawal@gmail.com>
2022-03-22 07:47:34 +0700

    Add pokemon querys

---
+++ src/generals/querys.js
@@ -0,0 +1,42 @@
+import { gql } from '@apollo/client';
+
+export const GET_POKEMONS = gql`
+  query getPokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      results {
+        id
+        name
+        image
+      }
+      count
+    }
+  }
+`;
+
+export const GET_POKEMON_DETAIL = gql`
+  query GetPokemonDetail($name: String!) {
+    pokemon(name: $name) {
+      abilities {
+        ability {
+          name
+        }
+        is_hidden
+      }
+      moves {
+        move {
+          name
+        }
+      }
+      weight
+      status
+      stats {
+        base_stat
+        stat {
+          id
+          url
+          name
+        }
+      }
+    }
+  }
+`;
